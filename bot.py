afb7f6d45093a845a5a8dfc14fbe5e832ef33e5b
unknown <nr9009@gmail.com>
2025-07-12 19:50:52 -0400

    v1.0

--- bot.py
+++ bot.py
@@ -1,17 +1,78 @@
 import os
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
+import aiohttp
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = 1393739742234939422  # Replace with your Discord channel ID
+COLLECTION_ADDRESS = 'AWDdLzHkvsaMcwSzEUMb3PxfQTVQhotYhXCk34ZyBuEY'
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+last_listing_ids = set()
+last_buy_ids = set()
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
+    track_nft_events.start()
+
+@tasks.loop(seconds=60)
+async def track_nft_events():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel is None:
+        print(f"Channel with ID {CHANNEL_ID} not found.")
+        return
+    async with aiohttp.ClientSession() as session:
+        # Track new listings
+        listings_url = f"https://api-mainnet.magiceden.dev/v2/collections/{COLLECTION_ADDRESS}/listings?offset=0&limit=10"
+        async with session.get(listings_url) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                global last_listing_ids
+                new_listings = []
+                for item in data:
+                    if item['id'] not in last_listing_ids:
+                        new_listings.append(item)
+                        last_listing_ids.add(item['id'])
+                if new_listings:
+                    for item in new_listings:
+                        msg = f"New Listing: {item['title']} for {item['price']} SOL\nLink: https://magiceden.io/item-details/{item['mintAddress']}"
+                        await channel.send(msg)
+                # Keep only the latest 50 IDs
+                if len(last_listing_ids) > 50:
+                    last_listing_ids = set(list(last_listing_ids)[-50:])
+            else:
+                print(f"Failed to fetch listings: {resp.status}")
+
+        # Track new buys (sales)
+        sales_url = f"https://api-mainnet.magiceden.dev/v2/collections/{COLLECTION_ADDRESS}/activities?offset=0&limit=20&type=buyNow"
+        async with session.get(sales_url) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                global last_buy_ids
+                new_buys = []
+                for item in data:
+                    if item['txId'] not in last_buy_ids:
+                        new_buys.append(item)
+                        last_buy_ids.add(item['txId'])
+                if new_buys:
+                    for item in new_buys:
+                        price = item.get('price', 'N/A')
+                        buyer = item.get('buyer', 'Unknown')
+                        mint = item.get('mint', '')
+                        msg = f"New Buy: {price} SOL by {buyer}\nLink: https://magiceden.io/item-details/{mint}"
+                        await channel.send(msg)
+                # Keep only the latest 50 IDs
+                if len(last_buy_ids) > 50:
+                    last_buy_ids = set(list(last_buy_ids)[-50:])
+            else:
+                print(f"Failed to fetch buys: {resp.status}")
 
 @bot.command()
 async def hello(ctx):
